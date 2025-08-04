2806c970c4f579451c415e7cb72fe558513bb970
ederramosh <eder.ramosh@gmail.com>
2025-08-04 21:22:15 +0000

    building the getBootcamp API

--- src/api/supabase.api.ts
+++ src/api/supabase.api.ts
@@ -7,3 +7,22 @@ export const supabaseApi = axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+export const getBootcamps = async () => {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/bootcamps`,
+      {
+        headers: {
+          apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    return response.data
+  } catch (error) {
+    console.log(error)
+    throw new Error("Ocurrió un error")
+  }
+}
